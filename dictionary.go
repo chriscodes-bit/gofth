a57cebd9bf0306a7b357354ef34ce1c46b49377e
bigsaddev <chris.doehmann@googlemail.com>
2026-01-18 20:38:25 +0100

    Added words(pi, sqrt, min, max, abs)

--- dictionary.go
+++ dictionary.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
@@ -110,6 +111,79 @@ var Builtins = map[string]Word{
 		Category:    "Math",
 		Execute: func() any {
 			binaryOp(func(a, b float64) float64 { return a / b })
+			return nil
+		},
+	},
+	"pi": {
+		Name:        "Pi",
+		Description: "Represents Pi as 3.141592653589793",
+		Category:    "Math",
+		Execute: func() any {
+			push(math.Pi)
+			return nil
+		},
+	},
+	"sqrt": {
+		Name:        "Square Root",
+		Description: "Calculates the square root of the top most element on the stack",
+		Category:    "Math",
+		Execute: func() any {
+			a := pop()
+			af, aok := a.(float64)
+			if !aok {
+				output.Write("Error: sqrt didn't receive a number")
+				return nil
+			}
+			push(math.Sqrt(af))
+			return nil
+		},
+	},
+	"min": {
+		Name:        "Minimum",
+		Description: "Returns the minimum of the two top most stack values",
+		Category:    "Math",
+		Execute: func() any {
+			a := pop()
+			b := pop()
+			af, aok := a.(float64)
+			bf, bok := b.(float64)
+			if !aok || !bok {
+				output.Write("Error: Not enough valid numbers")
+				return nil
+			}
+			push(math.Min(af, bf))
+			return nil
+		},
+	},
+	"max": {
+		Name:        "Maximum",
+		Description: "Returns the maximum of the two top most stack values",
+		Category:    "Math",
+		Execute: func() any {
+			a := pop()
+			b := pop()
+			af, aok := a.(float64)
+			bf, bok := b.(float64)
+			if !aok || !bok {
+				output.Write("Error: Not enough valid numbers")
+				return nil
+			}
+			push(math.Max(af, bf))
+			return nil
+		},
+	},
+	"abs": {
+		Name:        "Absolute",
+		Description: "Returns the absolute value of the top most stack value",
+		Category:    "Math",
+		Execute: func() any {
+			a := pop()
+			af, aok := a.(float64)
+			if !aok {
+				output.Write("Error: Not enough valid numbers")
+				return nil
+			}
+			push(math.Abs(af))
 			return nil
 		},
 	},
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # GoFth | A Toy Forth implemented in Go
 Personal project writing a Forth Interpreter and TUI-IDE in one.
 It doesn't do much yet, besides being a glorified calculator with an odd notation.
+
+## TODO
+[] Implement strings
+[] Implement booleans
+[] Implement control flow
